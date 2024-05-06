cb9cf2b13a29d46535fb45a35c2264b5b1ac1eef
ahmed <sdiria673@gmail.com>
2024-05-06 10:27:24 +0100

    first commit

--- src/test/java/createGame.java
+++ src/test/java/createGame.java
@@ -1,33 +1,50 @@
-import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
-import io.appium.java_client.touch.LongPressOptions;
-import io.appium.java_client.touch.TapOptions;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.ElementOption;
-import org.checkerframework.checker.units.qual.A;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.time.Duration;
-
 import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.android.nativekey.AndroidKey.*;
 import static org.openqa.selenium.By.xpath;
 
 public class createGame extends loginbasics{
+    WebDriver wait;
+    AppiumDriverLocalService appiumDriverLocalService;
+
 
     @BeforeMethod
     public void setUpDriver() throws URISyntaxException, MalformedURLException, InterruptedException {
+        appiumDriverLocalService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder());
+        appiumDriverLocalService.start();
         AppiumTest();
     }
+    @AfterMethod
+    public void tearDown() {
+        try {
+            Thread.sleep(5000); // Attendre 5 secondes
+            stopAppiumServer(); // Arrêter le serveur Appium
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopAppiumServer() {
+        if (appiumDriverLocalService != null) {
+            appiumDriverLocalService.stop();
+        }
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        driver.quit();
+    }
+
     @Test
     public void edit1() throws URISyntaxException, MalformedURLException {
         try {
@@ -59,27 +76,49 @@ public class createGame extends loginbasics{
            team2.sendKeys("Team2");
             driver.hideKeyboard();
             WebElement price= driver.findElement(xpath("//android.widget.ScrollView/android.view.View[8]"));
-            for(int i=1; i<12; i++) {
+            for(int i=0; i<5; i++) {
                 price.click();
             }
             driver.findElement(xpath("//android.widget.ScrollView/android.view.View[9]/android.widget.ImageView")).click();
-            driver.findElement(xpath("//android.widget.Button[@content-desc=\"Sélectionner une date\n" +
-                    "mar. 30 avr.\n" +
-                    "Sélectionner une année\"]/android.widget.Button[2]")).click();
-            driver.findElement(xpath("//android.widget.Button[@content-desc=\"1, mercredi 1 mai 2024\"]")).click();
+            driver.findElement(xpath("//android.widget.Button[@content-desc=\"7, mardi 7 mai 2024\"]")).click();
             driver.findElement(accessibilityId("OK")).click();
             driver.findElement(xpath("//android.widget.ScrollView/android.view.View[10]/android.widget.ImageView")).click();
-            WebElement hours = driver.findElement(AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button[1]"));
-            hours.click();
-            WebElement hour = driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text=\"11\"]"));
-            hour.click();
-            driver.pressKey(new KeyEvent(ENTER));
-            driver.pressKey(new KeyEvent(ENTER));
-            hours.sendKeys("12");
-            WebElement minutes = driver.findElement(accessibilityId("Sélectionner des minutes 32"));
-            minutes.click();
-            minutes.sendKeys("11");
-
+            WebElement hour = driver.findElement(xpath("//android.widget.SeekBar[@content-desc=\"Sélectionner une heure 09\"]"));
+            Actions action1 = new Actions(driver);
+            action1.moveToElement(hour).click().pause(100).click().perform();
+            Thread.sleep(2000);
+            driver.findElement(xpath("//android.widget.EditText[@text=\"09\"]"))
+                    .clear();
+            Thread.sleep(2000);
+            driver.findElement(xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"))
+                    .sendKeys("9");
+            driver.findElement(accessibilityId("OK")).click();
+            driver.findElement(xpath("//android.widget.ScrollView/android.view.View[11]/android.widget.ImageView")).click();
+            driver.findElement(xpath("//android.widget.Button[@content-desc=\"Sélectionner un emplacement\"]")).click();
+            WebElement max = driver.findElement(xpath("//android.widget.ScrollView/android.view.View[19]"));
+            for(int i = 0; i < 16; i++) {
+                max.click();
+            }
+            Thread.sleep(2000);
+            WebElement des = driver.findElement(xpath("//android.widget.EditText"));
+            des.click();
+            des.sendKeys("Test");
+            WebElement min = driver.findElement(xpath("//android.widget.ScrollView/android.view.View[15]"));
+            for(int i = 0; i < 1; i++) {
+                min.click();
+            }
+            Thread.sleep(2000);
+            boolean canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture",
+                    ImmutableMap.of("left", 100, "top", 100, "width", 200, "height", 1500, "direction", "up", "percent",
+                            2.0));
+            driver.findElement(xpath("//android.widget.Button[@content-desc=\"Partager\"]")).click();
+            Thread.sleep(1000);
+            boolean isDisplayed1 = driver.findElement(xpath("//android.view.View[@content-desc=\"Match créé avec succès\"]")).isDisplayed();
+            if (isDisplayed1) {
+                System.out.println("Match cree avec succes");
+            } else {
+                System.out.println("erreur de creation du match");
+            }
         } catch (Exception e) {
                 e.printStackTrace();
             }
